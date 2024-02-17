3f9548269d16b099b7090d8e4ca178ff1193a644
KasiaGrabska <143888097+KasiaGrabska@users.noreply.github.com>
2024-02-17 19:16:18 +0100

    Changes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
+import Notiflix from 'notiflix'; // Dodaj import biblioteki Notiflix
 import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 
-const fetchBreedsBtn = document.querySelector('.btn');
 const breedSelect = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
 const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
 
-fetchBreedsBtn.addEventListener('click', () => {
-  try {
-    loader.classList.remove('hidden');
-    fetchBreeds().then(data => renderSelect(data));
-  } catch (error) {
-    console.log(error);
-  }
-});
+error.classList.add('hidden');
+
+try {
+  loader.classList.remove('hidden');
+  fetchBreeds().then(data => renderSelect(data));
+} catch (error) {
+  console.log(error);
+}
 
 function renderSelect(breeds) {
   const markup = breeds
@@ -26,8 +27,15 @@ function renderSelect(breeds) {
 
 breedSelect.addEventListener('change', e => {
   loader.classList.remove('hidden');
-  fetchCatByBreed(e.target.value).then(data => renderCat(data[0]));
+  catInfo.innerHTML = ''; // Wyczyść zawartość catInfo przed wyświetleniem nowych danych
+  fetchCatByBreed(e.target.value)
+    .then(data => renderCat(data[0]))
+    .catch(() => {
+      Notiflix.Notify.failure('Error checking cat:');
+      loader.classList.add('hidden');
+    });
 });
+
 function renderCat(catData) {
   const { url } = catData;
   const { description, name, temperament } = catData.breeds[0];
