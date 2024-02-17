c5177b5e1fce27fbbac9296050ec94e41d18f294
KasiaGrabska <143888097+KasiaGrabska@users.noreply.github.com>
2024-02-17 17:53:54 +0100

    Changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+import axios from 'axios';
+import { fetchBreeds } from './cat-api.js';
+
 const fetchBreedsBtn = document.querySelector('.btn');
 const breedSelect = document.querySelector('.breed-select');
 
 fetchBreedsBtn.addEventListener('click', () => {
   try {
-    fetchBreedsBtn().then(data => renderSelect(data));
+    fetchBreeds().then(data => renderSelect(data));
   } catch (error) {
     console.log(error);
   }
@@ -19,5 +22,9 @@ function renderSelect(breeds) {
 }
 
 breedSelect.addEventListener('change', e => {
-  console.log(e.target.value);
+  axios
+    .get(
+      `https://api.thecatapi.com/v1/images/search?breed_ids=${e.target.value}`
+    )
+    .then(res => console.log(e.target.value));
 });
